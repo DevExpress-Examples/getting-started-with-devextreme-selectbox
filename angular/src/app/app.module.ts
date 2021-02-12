cb6129c3961d98efae7d1faf129ed38836486925
LexDX <42230789+LexDX@users.noreply.github.com>
2021-02-12 19:12:21 +0300

    fix imports

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import { DxSelectBoxModule } from 'devextreme-angular';
+import { DxSelectBoxModule } from 'devextreme-angular/ui/select-box';
 
 
 @NgModule({
