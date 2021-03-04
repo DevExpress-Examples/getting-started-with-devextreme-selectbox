b19a96b591ea5e06d1d991e360a918f8ef6ff40b
czernous-devexpress <anton.chernous@devexpress.com>
2021-03-04 13:06:03 +0300

    clean up the code

--- react/src/App.js
+++ react/src/App.js
@@ -9,39 +9,39 @@ import DataSource from 'devextreme/data/data_source';
 import { data } from './data';
 
 const dataSource = new DataSource({
-    store: {
-        data: data,
-        type: 'array',
-        key: 'ID'
-    },
-    group: "Category"
+  store: {
+    data: data,
+    type: 'array',
+    key: 'ID',
+  },
+  group: 'Category',
 });
 
 const dropDownOptions = {
-    height: 150,
-  },
+  height: 150,
+};
 
 class App extends React.Component {
-    onValueChanged(e) {
-        const item = data.filter(i => i.ID === e.value)[0];
-        console.log(item.ID + ": " + item.Name);
-    }
+  onValueChanged(e) {
+    const item = data.filter((i) => i.ID === e.value)[0];
+    console.log(item.ID + ': ' + item.Name);
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <SelectBox
-                    dataSource={dataSource}
-                    valueExpr="ID"
-                    displayExpr="Name"
-                    searchEnabled={true}
-                    onValueChanged={this.onValueChanged}
-                    grouped={true}
-                    dropDownOptions={dropDownOptions}
-                />
-            </div>
-        );        
-    }
+  render() {
+    return (
+      <div className="App">
+        <SelectBox
+          dataSource={dataSource}
+          valueExpr="ID"
+          displayExpr="Name"
+          searchEnabled={true}
+          onValueChanged={this.onValueChanged}
+          grouped={true}
+          dropDownOptions={dropDownOptions}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
